64a9525570a6526ac9a046f487557cd9d7c6a62d
btw <gavilon324@gmail.com>
2023-03-19 17:43:03 +0100

    commit

---
+++ main/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from main.models import User, Account, AccountEmail
+
+# Register your models here.
+
+
+@admin.register(User)
+class SettingAdmin(admin.ModelAdmin):
+    list_display = ('nick', 'password', 'email')
+    search_fields = ('nick',)
+
+
+@admin.register(Account)
+class SettingAccount(admin.ModelAdmin):
+    list_display = ('nick', 'password', 'email')
+    search_fields = ('nick',)
+
+
+@admin.register(AccountEmail)
+class SettingAccountEmail(admin.ModelAdmin):
+    list_display = ('email', 'adress', 'password', 'port', 'status')
+    search_fields = ('nick',)
